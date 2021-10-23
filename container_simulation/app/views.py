6d95cdda2162fdefe3e125d72fbc475179e156a3
ArrmenLee <wachemanstonphoenix@gmail.com>
2021-10-23 21:36:48 +0800

    (RESTFul)[A] Get container

--- container_simulation/app/views.py
+++ container_simulation/app/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from json import loads
 
 
 def index(request):
     return HttpResponse("Hello Nuts!")
+
+
+def container(request):
+    container_info = loads(request.GET['data'])
+    print(container_info)
--- container_simulation/container_simulation/urls.py
+++ container_simulation/container_simulation/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, container
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
+    path('container/', container),
 ]
